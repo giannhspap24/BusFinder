bab4e7f69d1eea39d2050c043fde0271914b7f8a
cgeopapa <cgeocodgod@gmail.com>
2019-03-21 18:44:53 +0200

    init

---
+++ untitled/src/Node.java
@@ -0,0 +1,25 @@
+
+public class Node
+{
+    public List<Brokers> brokers;
+
+    public  void init(int)
+    {
+
+    }
+
+    public void connect()
+    {
+
+    }
+
+    public void disconnect()
+    {
+
+    }
+
+    public void updateNodes()
+    {
+
+    }
+}
